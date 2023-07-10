e6978e00acdcbee171ef17b08d3ff6ba20ab2c7f
Cat <beligtobadluev@yandex.ru>
2023-07-10 22:53:06 +0300

    123

---
+++ test1.py
@@ -0,0 +1,11 @@
+a = [1,2,3,4,5,6,7,8,9]
+
+def main():
+    print('hello every body, do you want to play?\n1.yes 2.no')
+    start = input('1 or 2? - ')
+    if start:
+        flag = True
+        while flag:
+            print()
+
+main()
